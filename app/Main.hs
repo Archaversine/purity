f7e08dfdaa76186072defde872863ecc1ccbe16b
Archaversine <adam.brohl.w@gmail.com>
2024-03-11 10:48:20 -0500

    Added #import directive

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,56 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Main where
 
 import Control.Monad.IO.Class
+import Control.Monad.State
+
+import Data.List
 
 import System.IO
 
 import qualified Language.Haskell.Interpreter as Hint
 
-type Interpreter = Hint.InterpreterT IO
+newtype InterpreterState = IntState { intImports :: [String] }
 
-purityImports :: Interpreter ()
-purityImports = Hint.setImports ["Prelude"]
+instance Semigroup InterpreterState where 
+    IntState a <> IntState b = IntState (a <> b) 
+
+instance Monoid InterpreterState where 
+    mempty = IntState []
+
+type Interpreter = StateT InterpreterState (Hint.InterpreterT IO)
+
+instance Hint.MonadInterpreter Interpreter where
+    fromSession          = lift . Hint.fromSession
+    modifySessionRef t f = lift $ Hint.modifySessionRef t f
+    runGhc x             = lift $ Hint.runGhc x
+
+purityImport :: [String] -> Interpreter ()
+purityImport xs = do 
+    curr <- get 
+    let new@(IntState ys) = curr <> IntState xs
+    put new >> Hint.setImports ys
 
 purityLoop :: Interpreter ()
-purityLoop = liftIO (putStr "Purity > " >> getLine) >>= Hint.runStmt >> purityLoop
+purityLoop = liftIO (putStr "Purity > " >> getLine) >>= purityStmt >> purityLoop
+
+purityStmt :: String -> Interpreter ()
+purityStmt ('#' : xs) 
+    | "purge"  `isPrefixOf` xs = Hint.reset
+    | "import" `isPrefixOf` xs = purityImport (tail $ words xs)
+    | otherwise = liftIO $ putStrLn $ "Unknown directive: " ++ xs
+purityStmt xs = Hint.runStmt xs
 
 purity :: Interpreter ()
-purity = purityImports >> purityLoop
+purity = purityImport ["Prelude"] >> purityLoop
 
 main :: IO ()
 main = do 
     hSetBuffering stdout NoBuffering
     putStrLn "PURITY SHELL [ALPHA]\n"
 
-    result <- Hint.runInterpreter purity 
+    result <- Hint.runInterpreter (evalStateT purity mempty)
 
     case result of 
         Left err -> putStrLn $ "Error: " ++ show err

b3d98ed1e7e7e0f159be9e36beabb5029df1a733
Archaversine <adam.brohl.w@gmail.com>
2024-03-12 16:24:01 -0500

    Added string detection for command mode

--- app/Purity/Cmd.hs
+++ app/Purity/Cmd.hs
@@ -12,12 +12,20 @@ import Purity.Directive
 haskellSymbolChar :: Char -> Bool 
 haskellSymbolChar = (`elem` "!#$%&*+./<=>?@\\^|-~")
 
+-- Same as `words` but anything inside quotes is treated as a single word
+wordsAndStrings :: String -> [String] 
+wordsAndStrings [] = [] 
+wordsAndStrings ('"':xs) = let (word, rest) = break (== '"') xs in 
+    ('"' : word ++ "\"") : wordsAndStrings (dropWhile isSpace $ drop 1 rest)
+wordsAndStrings xs = let (word, rest) = break isSpace xs in
+    word : wordsAndStrings (dropWhile isSpace rest)
+
 formatInput :: String -> Purity String
 formatInput input 
     | isDirective input || input == "```" = return input
     | otherwise = gets (view (intSettings.termMode)) >>= \case
     CodeMode    -> return input 
-    CommandMode -> case words input of 
+    CommandMode -> case wordsAndStrings input of 
         []     -> return "" 
         [x]    -> return x
         (x:xs) -> return $ unwords (x : formatWords xs)
@@ -28,6 +36,7 @@ formatWords ([]:xs) = formatWords xs
 formatWords (x:xs) 
     | "." == x                = show x : next
     | ".." == x               = show x : next
+    | '"' == head x           = x      : next
     | all isNumber x          = x      : next
     | all haskellSymbolChar x = x      : next
     | isUpper (head x)        = x      : next

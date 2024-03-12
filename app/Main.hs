7411a50e2c5e8ac92804b2348b45dff4f377dc79
Archaversine <adam.brohl.w@gmail.com>
2024-03-12 14:59:46 -0500

    Added Purity module

--- app/Main.hs
+++ app/Main.hs
@@ -2,41 +2,11 @@
 
 module Main where
 
-import Control.Lens
-
-import Control.Monad.IO.Class
 import Control.Monad.State
 
-import Data.Maybe
-
-import Purity.Types
-import Purity.Imports 
-import Purity.Prompt
-import Purity.Stmt
+import Purity
 
 import System.IO
-
-purityLoop :: Purity ()
-purityLoop = promptUser >>= purityRunLine >> purityLoop
-
-purity :: Purity ()
-purity = do 
-    loadModules ["Config.hs", "User.hs"]
-    purityImport [ModuleImport "Config" (QualifiedAs (Just "Config")) NoImportList]
-    purityImport [ModuleImport "User"   NotQualified                  NoImportList]
-
-    prompt <- interpret "Config.shellPrompt" (as :: String)
-    errClr <- fromMaybe "" <$> interpret "Config.errorColorPrefix" (as :: Maybe String)
-    block  <- fromMaybe "" <$> interpret "Config.blockPrompt"      (as :: Maybe String)
-
-    let settings = defaultTermSettings & termPrompt .~ prompt
-                                       & termErrClr .~ errClr
-                                       & termBlock  .~ block
-
-    intSettings .= settings
-
-    interpret "Config.splashText"     (as :: Maybe String) >>= liftIO . putStrLn . fromMaybe ""
-    interpret "Config.defaultImports" (as :: [String]    ) >>= purityImportStr >> purityLoop
 
 main :: IO ()
 main = do 
---
+++ app/Purity.hs
@@ -0,0 +1,37 @@
+module Purity ( module Purity.Types 
+              , module Purity.Imports 
+              , module Purity.Prompt 
+              , module Purity.Stmt
+              , purity 
+              , purityLoop) where 
+
+import Control.Lens
+
+import Data.Maybe
+
+import Purity.Types
+import Purity.Imports 
+import Purity.Prompt
+import Purity.Stmt
+
+purityLoop :: Purity ()
+purityLoop = promptUser >>= purityRunLine >> purityLoop
+
+purity :: Purity ()
+purity = do 
+    loadModules ["Config.hs", "User.hs"]
+    purityImport [ModuleImport "Config" (QualifiedAs (Just "Config")) NoImportList]
+    purityImport [ModuleImport "User"   NotQualified                  NoImportList]
+
+    prompt <- interpret "Config.shellPrompt" (as :: String)
+    errClr <- fromMaybe "" <$> interpret "Config.errorColorPrefix" (as :: Maybe String)
+    block  <- fromMaybe "" <$> interpret "Config.blockPrompt"      (as :: Maybe String)
+
+    let settings = defaultTermSettings & termPrompt .~ prompt
+                                       & termErrClr .~ errClr
+                                       & termBlock  .~ block
+
+    intSettings .= settings
+
+    interpret "Config.splashText"     (as :: Maybe String) >>= liftIO . putStrLn . fromMaybe ""
+    interpret "Config.defaultImports" (as :: [String]    ) >>= purityImportStr >> purityLoop
